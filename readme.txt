db67f2eeb46aa63155b2a8df3ba6dfc7230a9f13
WP Wham <support@wpwham.com>
2019-09-14 01:44:34 -0700

    automatically bumping tested versions

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpwham
 Tags: woocommerce, currency, currencies, cryptocurrency, cryptocurrencies, woo commerce
 Requires at least: 4.4
 Tested up to: 5.2
-Stable tag: 2.3.0
+Stable tag: 2.3.1
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -39,6 +39,9 @@ In [Pro version](https://wpwham.com/products/all-currencies-for-woocommerce/) yo
 3. Start by visiting plugin settings at "WooCommerce > Settings > Currencies".
 
 == Changelog ==
+
+= 2.3.1 - 2019-09-14 =
+* UPDATE: bump tested versions
 
 = 2.3.0 - 2019-05-13 =
 * UPDATE: updated .pot file for translations
--- woocommerce-all-currencies.php
+++ woocommerce-all-currencies.php
@@ -3,13 +3,13 @@
 Plugin Name: All Currencies for WooCommerce
 Plugin URI: https://wpwham.com/products/all-currencies-for-woocommerce/
 Description: Add all countries currencies and cryptocurrencies to WooCommerce.
-Version: 2.3.0
+Version: 2.3.1
 Author: WP Wham
 Author URI: https://wpwham.com
 Text Domain: woocommerce-all-currencies
 Domain Path: /langs
 Copyright: © 2019 WP Wham
-WC tested up to: 3.6
+WC tested up to: 3.7
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -53,7 +53,7 @@ final class Alg_WC_All_Currencies {
 	 * @var   string
 	 * @since 2.1.0
 	 */
-	public $version = '2.3.0';
+	public $version = '2.3.1';
 
 	/**
 	 * @var Alg_WC_All_Currencies The single instance of the class
